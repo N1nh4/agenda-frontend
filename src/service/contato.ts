3a9c2f88fd2c833d097c643998ad0ae3609034d6
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-09-30 11:33:17 -0300

    contato

--- src/service/contato.ts
+++ src/service/contato.ts
@@ -1,14 +1,9 @@
 import { Contato } from "@/core/contato";
 
-// Função auxiliar para pegar token JWT
-function getToken() {
-    return localStorage.getItem("jwt");
-}
-
 async function getContatosService(idAgenda?: string): Promise<Contato[] | undefined> {
-    if (!idAgenda) return;
-    const token = getToken();
-    if (!token) return;
+    if (!idAgenda) {
+        return
+    }
 
     const idAgendaInt = parseInt(idAgenda);
 
@@ -16,125 +11,125 @@ async function getContatosService(idAgenda?: string): Promise<Contato[] | undefi
         const resposta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/minha_agenda/contatos/agenda/${idAgendaInt}`, {
             method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        });
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        })
 
-        if (!resposta.ok) return;
+        if (!resposta.ok) {
+            return
+        }
 
         return await resposta.json();
-    } catch (e) {
-        return;
+    } catch(e) {
+        return
     }
 }
 
 async function salvarContatoService(idAgenda: string, nome: string, telefone: string): Promise<number | undefined> {
-    const token = getToken();
-    if (!token) return;
-
     try {
         const resposta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/minha_agenda/contatos`, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
+                "Content-Type": "application/json"
             },
-            body: JSON.stringify({ idAgenda, nome, telefone })
-        });
+            credentials: "include",
+            body: JSON.stringify({idAgenda, nome, telefone})
+        })
 
-        if (!resposta.ok) return;
 
-        return resposta.status;
-    } catch (e) {
-        return;
+        if (!resposta.ok) {
+            return
+        }
+
+        return resposta.status
+    } catch(e) {
+        return
     }
 }
 
 async function atualizarContatoService(id: string | undefined, nome: string, telefone: string): Promise<number | undefined> {
-    if (!id) return;
-    const token = getToken();
-    if (!token) return;
-
     try {
         const resposta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/minha_agenda/contatos/${id}`, {
             method: "PUT",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
+                "Content-Type": "application/json"
             },
-            body: JSON.stringify({ nome, telefone })
-        });
+            credentials: "include",
+            body: JSON.stringify({nome, telefone})
+        }
+        )
 
-        if (!resposta.ok) return;
+        if (!resposta.ok) {
+            return
+        }
 
-        return resposta.status;
-    } catch (e) {
-        return;
+        return resposta.status
+    } catch(e) {
+        return
     }
 }
 
 async function excluirContatoService(id: string | undefined): Promise<number | undefined> {
-    if (!id) return;
-    const token = getToken();
-    if (!token) return;
-
     try {
         const resposta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/minha_agenda/contatos/${id}`, {
             method: "DELETE",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        });
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        })
 
-        if (!resposta.ok) return;
+        if (!resposta.ok) {
+            return
+        }
 
-        return resposta.status;
-    } catch (e) {
-        return;
+        return resposta.status
+    } catch(e) {
+        return
     }
 }
 
 async function buscarContatosPorIdsService(busca: string): Promise<Contato[] | undefined> {
-    const token = getToken();
-    if (!token) return;
-
     try {
         const resposta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/minha_agenda/contatos/filtrar/${busca}`, {
             method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        });
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        })
 
-        if (!resposta.ok) return;
+        if (!resposta.ok) {
+            return;
+        }
 
         const dados: Contato[] = await resposta.json();
         return dados;
-
+        
     } catch (e) {
         return;
     }
 }
 
 async function deletarContatosPorIdsService(ids: number[]): Promise<number | undefined> {
-    if (ids.length === 0) return 204;
-    const token = getToken();
-    if (!token) return;
+    if (ids.length === 0) {
+        return 204; // No Content
+    }    
 
     try {
         const resposta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/minha_agenda/contatos/deletar/ids`, {
             method: "DELETE",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
+                "Content-Type": "application/json"
             },
+            credentials: "include",
             body: JSON.stringify(ids)
-        });
+        })
 
-        if (!resposta.ok) return;
+        if (!resposta.ok) {
+            return;
+        }
 
         return resposta.status;
     } catch (e) {
@@ -149,4 +144,4 @@ export {
     excluirContatoService,
     buscarContatosPorIdsService,
     deletarContatosPorIdsService
-};
+}
